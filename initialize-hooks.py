ba76c1c5029c4cf2f951ad9fed842e57886c6b76
Jeff Newman <jeffnewman@camsys.com>
2021-11-19 11:38:50 -0600

    init hooks

---
+++ initialize-hooks.py
@@ -0,0 +1,24 @@
+
+import os
+
+PRECOMMIT_REPLACER_TEMPLATE = r"""
+- search: /[/\\]{search_term}[/\\]/
+  replacement: ./
+"""
+
+def rec_split(s):
+    rest, tail = os.path.split(s)
+    if rest in ('', os.path.sep):
+        return tail,
+    return rec_split(rest) + (tail,)
+
+if __name__ == "__main__":
+
+    git_repo = os.path.dirname(__file__)
+    repo_split = rec_split(git_repo) + ("benchmarks",)
+    search_term = r"[/\\]".join(repo_split)
+    with open(".pre-commit-search-and-replace.yaml", 'wt') as f:
+        f.write(PRECOMMIT_REPLACER_TEMPLATE.format(
+            search_term=search_term,
+            pathsep=os.path.sep,
+        ))
