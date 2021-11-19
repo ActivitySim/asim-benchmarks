460da88e398fd53fb049af45f661e0a8273ee736
Jeff Newman <jeffnewman@camsys.com>
2021-11-19 10:56:41 -0800

    blacken

--- initialize-hooks.py
+++ initialize-hooks.py
@@ -1,4 +1,3 @@
-
 import os
 
 PRECOMMIT_REPLACER_TEMPLATE = r"""
@@ -6,21 +5,25 @@ PRECOMMIT_REPLACER_TEMPLATE = r"""
   replacement: ./
 """
 
+
 def rec_split(s):
     rest, tail = os.path.split(s)
-    if rest in ('', os.path.sep):
-        return tail,
+    if rest in ("", os.path.sep):
+        return (tail,)
     if rest == s:
-        return rest,
+        return (rest,)
     return rec_split(rest) + (tail,)
+
 
 if __name__ == "__main__":
 
     git_repo = os.path.dirname(__file__)
     repo_split = rec_split(git_repo) + ("benchmarks",)
     search_term = r"[/\\]".join(repo_split)
-    with open(".pre-commit-search-and-replace.yaml", 'wt') as f:
-        f.write(PRECOMMIT_REPLACER_TEMPLATE.format(
-            search_term=search_term,
-            pathsep=os.path.sep,
-        ))
+    with open(".pre-commit-search-and-replace.yaml", "wt") as f:
+        f.write(
+            PRECOMMIT_REPLACER_TEMPLATE.format(
+                search_term=search_term,
+                pathsep=os.path.sep,
+            )
+        )
